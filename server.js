074e1362074ce81da5598b00af50fe3fce686792
SUDHAKAR1202 <sudhakargmk12@gmail.com>
2024-11-28 13:05:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/submit', (req, res) => {
+    console.log(req.body);
+
+    res.json({
+        message:'Data received successfully',
+        receivedData: req.body
+    })
+}
+);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
